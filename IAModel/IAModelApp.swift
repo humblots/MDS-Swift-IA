f09dbec2631e5598ec9750f1ab26f50e0ca83e0c
humblots <shumblot25@gmail.com>
2024-01-10 15:39:16 +0100

    Initial Commit

---
+++ IAModel/IAModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAModelApp.swift
+//  IAModel
+//
+//  Created by HUMBLOT Stéphane on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct IAModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
